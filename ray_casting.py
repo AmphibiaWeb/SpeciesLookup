009539eddb0a7ee36177f542f6c76117094b7561
Chenyu-Shi <shounshi2017@gmail.com>
2018-09-13 16:04:15 -0700

    ray casting methods

--- ray_casting.py
+++ ray_casting.py
@@ -7,4 +7,18 @@ class edge:
         self.a=pointa
         self.b=pointb
     def intersect(self,point):
-        # ray casting method here 
+        # Let's assume the ray always goes straight down
+        hit = 0
+        # ray casting method here
+        return hit
+    
+class polygon:
+    def __init__(points):
+        # convert points to a list of edges
+        self.edges=["to do"]
+    def is_inside(query_point):
+        hits=sum([edge.intersect(query_point) for edge in self.edges])
+        if hits%2==1:
+            # odd number of intersections
+            return True
+        return False
