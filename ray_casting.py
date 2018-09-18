1a7bbfe741e28355308036f43492d95c6d169fa5
Chenyu-Shi <shounshi2017@gmail.com>
2018-09-18 16:13:59 -0700

    add a few testing methods

--- ray_casting.py
+++ ray_casting.py
@@ -17,7 +17,7 @@ class edge:
         if max(self.a.x,self.b.x)<point.x:
             return hit
         projected_y= self.a.y + (self.b.y-self.a.y)/(self.b.x-self.a.x)*(point.x-self.a.x)
-        if project_y <= point.y:
+        if projected_y <= point.y:
             if self.a.x==point.x or self.b.x==point.x:
                 # Have to account for overcounting
                 hit=0.5
@@ -25,14 +25,17 @@ class edge:
             else:
                 hit=1
                 return hit
+        else:
+            return hit
 
 class polygon:
-    def __init__(points):
+    def __init__(self,points):
         # convert points to a list of edges
         self.edges=[edge(points[i],points[(i+1)%len(points)]) for i in range(len(points))]
-    def is_inside(query_point):
+    def is_inside(self,query_point):
         hits=sum([edge.intersect(query_point) for edge in self.edges])
         if hits%2==1:
             # odd number of intersections
             return True
-        return False
+        else:
+            return False
---
+++ viz.py
@@ -0,0 +1,26 @@
+import matplotlib
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import matplotlib.pyplot as plt
+import numpy as np
+import ray_casting as r
+
+coord=np.array([[0.2,0.2],[0.2,0.9],[0.9,0.2]])
+test=r.points(0.9,0.5)
+
+ps=[r.points(dot[0],dot[1]) for dot in coord]
+poly=r.polygon(ps)
+
+fig,ax=plt.subplots()
+
+patches=[]
+num_polygon=1
+polygon=Polygon(coord,closed=True)
+patches.append(polygon)
+p=PatchCollection(patches,cmap=matplotlib.cm.jet,alpha=0.4)
+
+ax.add_collection(p)
+plt.plot([test.x],[test.y])
+plt.title("tested point ("+str(test.x)+" ,"+str(test.y)+
+    ") is contained in polygon"+str(poly.is_inside(test)))
+plt.show()
