3d172c00e7e588d60726665c8e6bfe73f68556e5
Chenyu-Shi <shounshi2017@gmail.com>
2018-10-04 20:17:02 -0700

    saving everything

--- debug.py
+++ debug.py
@@ -1,9 +1,15 @@
 # This tests the ray_casting module
 import os 
+import time 
 import kmlparserclass as k
 import ray_casting as r
 point=r.points(-7.2,6)
-par = k.parser("Acanthixalus_sonjae")
-par.visualize(point)
+all_species = [name.split('.')[0] for name in next(os.walk('kml'))[1] if name]
 
-print(par.inside(point))
+accoutants = [k.parser(specie) for specie in all_species]
+
+start = time.clock()
+result = [account.scientific_name for account in accoutants if account.inside(point)]
+stop = time.clock()
+print(stop-start)
+print(result)
